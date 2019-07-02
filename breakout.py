c46cea29ca4ade9902f84d3cf21e79258efc0555
Ryan Smith <ryan.l.smith4@gmail.com>
2019-07-02 13:38:02 -0700

    breakout

---
+++ breakout.py
@@ -0,0 +1,11 @@
+def find_middle(ll):
+    middle_num = ll.count // 2
+    count = 0
+    current_node = ll.head
+    while current_node != None:
+        if counter = mid_num:
+            break
+        current_node = current_node.next
+        counter += 1
+
+    return (current_node.data, current_node.next.data)
